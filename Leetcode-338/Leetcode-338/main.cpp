5548edfe7bce09d7d6c3d0199c2b47d957786271
Jing Luo <leojing1989@gmail.com>
2022-03-30 20:56:06 +1100

    338. Counting Bits - 汉明重量n&(n-1)，依次计算出1的个数

---
+++ Leetcode-338/Leetcode-338/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Leetcode-338
+//
+//  Created by Jing Luo on 3/30/22.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> countBits(int n) {
+    vector<int> result;
+    for (int i = 0; i <= n; i ++) {
+        int temp = i;
+        int count = 0;
+        while (temp) {
+            temp = temp & (temp-1);
+            count ++;
+        }
+        result.emplace_back(count);
+    }
+    return result;
+}
+
+int main(int argc, const char * argv[]) {
+    vector<int> result = countBits(5);
+    std::cout << "\n";
+    return 0;
+}
+
+// time: O(n), space: O(1)
