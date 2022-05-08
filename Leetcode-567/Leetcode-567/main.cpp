8c4ff81dd6ed1716b462f3235b0ad288dbaaea4f
Jing Luo <leojing1989@gmail.com>
2022-05-08 23:23:56 +1000

    567. Permutation in String - sliding window

--- Leetcode-567/Leetcode-567/main.cpp
+++ Leetcode-567/Leetcode-567/main.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+/*
 void removeCharIn(string& s, char a) {
     for (int i = 0; i < s.length(); i ++) {
         if (s[i] == a) {
@@ -48,11 +49,35 @@ bool checkInclusion(string s1, string s2) {
         start ++;
     }
     return false;
+}*/
+
+
+bool checkInclusion(string s1, string s2) {
+    int n = s1.length(), m = s2.length();
+    if (n > m) {
+        return false;
+    }
+    vector<int> cnt1(26), cnt2(26);
+    for (int i = 0; i < n; ++i) {
+        ++cnt1[s1[i] - 'a'];
+        ++cnt2[s2[i] - 'a'];
+    }
+    if (cnt1 == cnt2) {
+        return true;
+    }
+    for (int i = n; i < m; ++i) {
+        ++cnt2[s2[i] - 'a'];
+        --cnt2[s2[i - n] - 'a'];
+        if (cnt1 == cnt2) {
+            return true;
+        }
+    }
+    return false;
 }
                                                                      
 int main(int argc, const char * argv[]) {
-    string s1 = "adsc";
-    string s2 = "dcda";
+    string s1 = "soosg";
+    string s2 = "dydsoosoosg";
     std::cout << checkInclusion(s1, s2) << "\n";
     return 0;
 }
