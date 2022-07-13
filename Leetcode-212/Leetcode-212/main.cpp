ec5240dfb569cda6f2f2460ef330b7ec148935a2
Jing Luo <leojing1989@gmail.com>
2022-07-13 22:05:09 +1000

    212. Word Search II - Trie, 注意很容易超时，要尽量简化dfs

--- Leetcode-212/Leetcode-212/main.cpp
+++ Leetcode-212/Leetcode-212/main.cpp
@@ -7,103 +7,65 @@
 
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
+#include <unordered_set>
 
 using namespace std;
 
-class TrieNode {
-public:
+struct TrieNode{
+    string word = "";
     TrieNode* next[26];
-    bool isWord;
-    TrieNode() {
-        memset(next, NULL, sizeof(next));
-        isWord = false;
-    }
-    ~TrieNode() {
-        for (int i = 0; i < 26; i ++) {
-            if (next[i]) {
-                delete next[i];
-            }
-        }
-    }
 };
 
-class Trie {
-    TrieNode* root;
-public:
-    TrieNode* getRoot(){return root;}
-
-    /** Initialize your data structure here. */
-    Trie(vector<string>& words) {
-        root = new TrieNode();
-        for (int i = 0; i < words.size(); i ++) {
-            insert(words[i]);
+TrieNode* root;
+    
+void addWord(const string& word) {
+    TrieNode* p = root;
+    for (int i = 0; i < word.size(); ++ i) {
+        if (!p->next[word[i] - 'a']) {
+            p->next[word[i] - 'a'] = new TrieNode();
         }
+        p = p->next[word[i] - 'a'];
     }
+    p->word = word;
+}
 
-    void insert(string word) {
-        TrieNode* p = root;
-        for (int i = 0; i < word.size(); i ++) {
-            if (p->next[word[i] - 'a'] == NULL) {
-                p->next[word[i] - 'a'] = new TrieNode();
-            }
-            p = p->next[word[i] - 'a'];
-        }
-        p->isWord = true;
+void findWords(vector<vector<char>>& board, int x, int y, TrieNode* root, vector<string>& result){
+    if(x<0||x>=board.size()||y<0||y>=board[0].size()) {
+        return;
     }
-
-    bool search(string word) {
-        TrieNode* p = root;
-        for (int i = 0; i < word.size() && p; i ++) {
-            p = p->next[word[i] - 'a'];
-        }
-        return p && p->isWord;
+    char c = board[x][y];
+    if (c == '*' || !root->next[c - 'a']) { // already visited or not fund in Trie
+        return;
     }
-
-    bool startWith(string prefix) {
-        TrieNode* p = root;
-        for (int i = 0; i < prefix.size() && p; i ++) {
-            p = p->next[prefix[i] - 'a'];
-        }
-        return p;
+    
+    root = root->next[c- 'a'];
+    if (root->word != "") {  // found one
+        result.emplace_back(root->word);
+        root->word = ""; // de-duplicate
     }
-};
-
-void help(int x, int y, vector<vector<char>>& board, TrieNode* root, string current, set<string>& result) {
-    if(x<0||x>=board.size()||y<0||y>=board[0].size() || board[x][y]==' ') return;
-    if(root->next[board[x][y]-'a'] != NULL){
-        current=current+board[x][y];
-        root=root->next[board[x][y]-'a'];
-        if(root->isWord) result.insert(current);
-        char c=board[x][y];
-        board[x][y]=' ';
-        help(x+1, y, board, root, current, result);
-        help(x-1, y, board, root, current, result);
-        help(x, y+1, board, root, current, result);
-        help(x, y-1, board, root, current, result);
-        board[x][y]=c;
-    }
+    board[x][y] = '*';
+    findWords(board, x+1, y, root, result);
+    findWords(board, x-1, y, root, result);
+    findWords(board, x, y+1, root, result);
+    findWords(board, x, y-1, root, result);
+    board[x][y] = c;
 }
 
 vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-    int row = int(board.size());
-    int col = 0;
-    if (row > 0) {
-        col = int(board[0].size());
+    root = new TrieNode();
+    for (int i = 0; i < words.size(); ++ i) {
+        addWord(words[i]);
     }
-    Trie* trie = new Trie(words);
-    TrieNode* root = trie->getRoot();
-    set<string> result;
-    for (int i = 0; i < row; i ++) {
-        for (int j = 0; j < col; j ++) {
-            string current = "";
-            help(i, j, board, root, current, result);
+    vector<string> result;
+    
+    for (int i = 0; i < board.size(); ++ i) {
+        for (int j = 0; j < board[0].size(); ++ j) {
+            findWords(board, i, j, root, result);
         }
     }
-    vector<string> res(result.begin(), result.end());
-    return res;
+    return result;
 }
+
 
 int main(int argc, const char * argv[]) {
     vector<vector<char>> board = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}}; //{{'a','b'}, {'c', 'd'}};
